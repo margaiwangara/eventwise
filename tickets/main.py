c2d0e168d3897754ea727072cf9d622bb20966a5
margaiwangara <margaiwangara@gmail.com>
2023-08-09 18:22:59 +0300

    feat: model ticket

--- tickets/main.py
+++ tickets/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from models import ticket
+from database import engine
+from routers import tickets
 
 app = FastAPI()
+
+app.include_router(tickets.router, prefix="/api")
+
+# Base ticket model
+ticket.Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
--- tickets/models/ticket.py
+++ tickets/models/ticket.py
@@ -1,10 +1,26 @@
-from sqlalchemy import Column, Integer, DateTime
+from sqlalchemy import Column, String, Integer, DateTime, Float
 from database import Base
 from sqlalchemy.sql import func
+from pydantic import BaseModel, Field
 
 
 class Ticket(Base):
     __tablename__ = "tickets"
 
     id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    title = Column(String)
+    price = Column(Float, default=0.00, )
     created_at = Column(DateTime, default=func.now())
+
+
+class CreateTicketRequest(BaseModel):
+    title: str = Field(min_length=5)
+    price: float = Field(gt=0)
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "ticket": "SolFest",
+                "price": 20000.00
+            }
+        }
